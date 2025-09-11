64b65dc4ea7cb832be060e4a8f9b516c080bbca7
Abhishek Raj Poudel <abhishekrajpoudel@gmail.com>
2025-09-11 11:06:46 +0545

    latest built file

---
+++ public/build/assets/rivers-LIpIYNTy.js
@@ -0,0 +1,16 @@
+import{c as t}from"./createLucideIcon-DrG5fpaa.js";import{j as a}from"./app-DZah_XuT.js";import{c as o,S as c,e as d}from"./index-pclV0e8S.js";/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const l=[["path",{d:"M7 16.3c2.2 0 4-1.83 4-4.05 0-1.16-.57-2.26-1.71-3.19S7.29 6.75 7 5.3c-.29 1.45-1.14 2.84-2.29 3.76S3 11.1 3 12.25c0 2.22 1.8 4.05 4 4.05z",key:"1ptgy4"}],["path",{d:"M12.56 6.6A10.97 10.97 0 0 0 14 3.02c.5 2.5 2 4.9 4 6.5s3 3.5 3 5.5a6.98 6.98 0 0 1-11.91 4.97",key:"1sl1rz"}]],b=t("Droplets",l);/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const u=[["path",{d:"M14 4v10.54a4 4 0 1 1-4 0V4a2 2 0 0 1 4 0Z",key:"17jzev"}]],h=t("Thermometer",u);/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const g=[["polyline",{points:"22 7 13.5 15.5 8.5 10.5 2 17",key:"126l90"}],["polyline",{points:"16 7 22 7 22 13",key:"kwv8wd"}]],x=t("TrendingUp",g);function w({className:e,...r}){return a.jsx("div",{"data-slot":"card",className:o("bg-card text-card-foreground flex flex-col gap-6 rounded-xl border py-6 shadow-sm",e),...r})}const v=d("inline-flex items-center justify-center rounded-md border px-2 py-0.5 text-xs font-medium w-fit whitespace-nowrap shrink-0 [&>svg]:size-3 gap-1 [&>svg]:pointer-events-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive transition-[color,box-shadow] overflow-auto",{variants:{variant:{default:"border-transparent bg-primary text-primary-foreground [a&]:hover:bg-primary/90",secondary:"border-transparent bg-secondary text-secondary-foreground [a&]:hover:bg-secondary/90",destructive:"border-transparent bg-destructive text-white [a&]:hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40",outline:"text-foreground [a&]:hover:bg-accent [a&]:hover:text-accent-foreground"}},defaultVariants:{variant:"default"}});function y({className:e,variant:r,asChild:n=!1,...s}){const i=n?c:"span";return a.jsx(i,{"data-slot":"badge",className:o(v({variant:r}),e),...s})}const k=e=>{switch(e){case"low":return"water-low";case"normal":return"water-normal";case"high":return"water-high";case"critical":return"water-critical";default:return"water-normal"}},L=e=>{switch(e){case"low":return"Low Level";case"normal":return"Normal Level";case"high":return"High Level";case"critical":return"Critical Level";default:return"Normal Level"}};export{y as B,w as C,b as D,x as T,k as a,h as b,L as g};
